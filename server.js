e056293fdac51e53853160dd90695faef8dbae53
Alex Allen <completenutter2@gmail.com>
2015-09-25 23:41:18 +0100

    Server will keep track of statements

--- server.js
+++ server.js
@@ -15,31 +15,38 @@ var logger      = require('./modules/Logger');
 // Init SignalRJs
 var signalR = SignalRJS();
 
+var state = {
+    type: 'init',
+    statements: [
+        {
+            Message : "Test statement A",
+            User : "user1",
+            Timestamp: new Date()
+        },
+        {
+            Message : "Test statement B",
+            User: "user2",
+            Timestamp: new Date(new Date().getTime() + (20*60*1000))
+        }
+    ]
+}
+
 //Create the hub connection
 //NOTE: Server methods are defined as an object on the second argument
 signalR.hub('blueApp',{
     broadcast : function(fromUserName, message){
+        var json = JSON.parse(message);
+
+        if(json.type == 'statement'){
+            var statement = json.statement;
+            state.statements.unshift(statement);
+        }
+
 		this.clients.all.invoke('onTransmit').withArgs([fromUserName,message])
 		console.log('broadcasting:'+message);
 	},
 	sendToUser : function(fromUserName, toUserName, message){
         if(toUserName == 'server' && message == 'init'){
-            var state = {
-                type: 'init',
-                statements: [
-                    {
-                        Message : "Test statement A",
-                        User : "user1",
-                        Timestamp: new Date()
-                    },
-                    {
-                        Message : "Test statement B",
-                        User: "user2",
-                        Timestamp: new Date(new Date().getTime() + (20*60*1000))
-                    }
-                ]
-            }
-
             var stateString = JSON.stringify(state);
 
             this.clients.user(fromUserName).invoke('onTransmit').withArgs(['server', stateString])
