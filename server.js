262be4aa9058162b819be373dc5f75ac5154f03e
Alex Allen <completenutter2@gmail.com>
2015-09-26 01:24:13 +0100

    Added local storage for client
    Generate guids serverside for each statement

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ var helpers = require('./modules/helpers');
 var config      = require('./modules/config');
 var logger      = require('./modules/Logger');
 
+function generateUUID() {
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+};
 
 // signalR server
 
@@ -36,11 +45,13 @@ var state = {
     type: 'init',
     statements: [
         {
+            Id : generateUUID(),
             Message : "Test statement A",
             User : "user1",
             Timestamp: new Date()
         },
         {
+            Id : generateUUID(),
             Message : "Test statement B",
             User: "user2",
             Timestamp: new Date(new Date().getTime() + (20*60*1000))
@@ -56,6 +67,9 @@ signalR.hub('blueApp',{
 
         if(json.type == 'statement'){
             var statement = json.statement;
+
+            statement.Id = generateUUID();
+
             state.statements.unshift(statement);
         }
 
