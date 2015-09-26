59ba8126626db3896b856a5e86e85bb094fd5ea4
Alex Allen <completenutter2@gmail.com>
2015-09-26 12:45:40 +0100

    Changed default messages in time for demo

--- modules/state.js
+++ modules/state.js
@@ -5,24 +5,24 @@ module.exports = {
     statements: [
         {
             id : helpers.generateUUID(),
-            message : "Test statement A",
-            user : "user1",
+            message : "I am really tired, I just want to go home",
+            user : "Alex",
             timestamp: new Date(),
             emojiCount: 0,
             messageCount: 0
         },
         {
             id : helpers.generateUUID(),
-            message : "Test statement B",
-            user: "user2",
+            message : "This has been a really long weekend so far, people just don't get how much I miss my bed :(",
+            user: "Kryszta",
             timestamp: new Date(new Date().getTime() + (10*60*1000)),
             emojiCount: 0,
             messageCount: 0
         },
         {
             id : helpers.generateUUID(),
-            message : "Test statement C",
-            user: "user1",
+            message : "My feet hurt",
+            user: "Alex",
             timestamp: new Date(new Date().getTime() + (20*60*1000)),
             emojiCount: 0,
             messageCount: 0
